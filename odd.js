397abc113a929ff56ce9247cb36d689b98b8f7b9
Hamim Ahmed <hamim.ahmed484@gmail.com>
2023-09-27 20:48:01 +0600

    more js

--- odd.js
+++ odd.js
@@ -1,4 +1,5 @@
-var number = 0;
+//Printint all odd number betwwe 1-20.
+var number = 1;
 while (number <= 20) {
     console.log(number);
     number = number + 2;
--- reverse.js
+++ reverse.js
@@ -3,3 +3,9 @@ while (num >= 1) {
     console.log(num);
     num--;
 }
+
+//For LOOP
+
+for (var i = 10; i>= 1;i--){
+    console.log(i);
+}
